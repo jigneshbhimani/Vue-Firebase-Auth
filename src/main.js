9d95e0b8f0fb80b2c2229c2672eb79db262ef258
jigneshbhimani <jigneshbhimani2411@gmail.com>
2022-07-08 13:54:09 +0530

    Vue Firebase Auth

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
 import BootstrapVue from "bootstrap-vue/dist/bootstrap-vue.esm";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import "bootstrap/dist/css/bootstrap.css";
@@ -30,6 +29,5 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   render: (h) => h(App),
 }).$mount("#app");
