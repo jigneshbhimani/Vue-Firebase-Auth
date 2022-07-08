2b6899b214489fcf36b85895c7450fadc894eaa6
jigneshbhimani <jigneshbhimani2411@gmail.com>
2022-07-08 14:02:14 +0530

    Vue Firebase Auth

--- src/main.js
+++ src/main.js
@@ -9,13 +9,13 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCJ0CyvLRfDNV85381s3f3oX4qcGf05x08",
-  authDomain: "vue-firebase-auth-50a19.firebaseapp.com",
-  projectId: "vue-firebase-auth-50a19",
-  storageBucket: "vue-firebase-auth-50a19.appspot.com",
-  messagingSenderId: "124042734641",
-  appId: "1:124042734641:web:d608f7fc23ab8ce3f33910",
-  measurementId: "G-Q7B4W0DZ84",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
